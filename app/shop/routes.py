c81d558e15558005e8dc4733c0e263b9e20bc914
Bo McCarthy <sysrock@gmail.com>
2022-10-31 16:13:06 -0500

    expanded products table and minor mods

--- app/shop/routes.py
+++ app/shop/routes.py
@@ -8,9 +8,7 @@ shop = Blueprint('shop', __name__, template_folder='shop_templates')
 @shop.route('/product/<int:product_id>')
 def showProduct(product_id):
     singleProduct = getProd(product_id)
-
     return render_template('singleProduct.html', singleProduct=singleProduct)
-
 
 @shop.route('/cart')
 def showCart():
@@ -23,29 +21,24 @@ def showCart():
     # print(count_of_all_products)
     # print(sum_of_all_products)
     # count_of_product = Product.query.join(cartTable).join(Customer).get()
-
     return render_template('cart.html', viewCart=viewCart)
 
 @shop.route('/add/<int:product_id>')
 def addProduct(product_id):
     if Product.query.get(product_id):
-
         product = Product.query.get(product_id)
-    
         current_user.addToCart(product)
-
     else:
         product = getProd(product_id)
         name = product['title']
-        description = product['description']
         price = product['price']
-
-        new_product = Product(name, description, price)
-
+        description = product['description']
+        category = product['category']
+        image = product['image']
+        rating = product['rating']
+        count = product['count']
+        new_product = Product(name, price, description, category, image, rating, count)
         new_product.saveToDB()
-
-
         current_user.addToCart(new_product)
-
+        return redirect(url_for('shop.showCart'))
     return redirect(url_for('shop.showCart'))
-
